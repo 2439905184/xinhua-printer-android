0127502f3dae1fb26f50b1250b5554f11f7b4e9f
2439905184 <suchenglong018@126.com>
2022-06-06 20:14:03 +0800

    🐞 fix: 修复闪退bug

---
+++ app/src/main/java/net/printer/printdemo16/CustomR58.java
@@ -0,0 +1,13 @@
+package net.printer.printdemo16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomR58 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_r58);
+    }
+}
